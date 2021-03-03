5baaf435e9093c3fac366c0797a67b3b79e0da44
filipenyakaterina <katerina.filipenya@tut.by>
2021-03-03 17:14:55 +0300

    add realization of Hardcore Task: add page to get price calculation results

---
+++ src/test/java/tasks/hardcore/page/PriceCalculationResult.java
@@ -0,0 +1,38 @@
+package tasks.hardcore.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import tasks.hardcore.helper.Executor;
+import tasks.hardcore.helper.Switcher;
+
+public class PriceCalculationResult extends AbstractPage {
+
+    @FindBy(xpath = "//md-card-content[@id='resultBlock']//button[@id='email_quote']")
+    private WebElement emailEstimateButton;
+
+    @FindBy(xpath = "//md-card-content[@id='resultBlock']//b[contains(text(), 'Total Estimated Cost:')]")
+    private WebElement estimateCost;
+
+    protected PriceCalculationResult(WebDriver driver) {
+        super(driver);
+        if (!driver.getCurrentUrl().contains(GoogleCloudPlatformPricingCalculatorPage.CALCULATOR_URL)) {
+            throw new IllegalStateException("This is not the Google Cloud Platform Pricing Calculator page!");
+        }
+        Switcher.switchToFrame();
+    }
+
+    public AbstractPage openPage() {
+        throw new RuntimeException("Cannot open page with the result of price calculation without providing data entry to the Google Cloud Platform Pricing Calculator.");
+    }
+
+    public EmailEstimatePage clickEmailEstimate() {
+        Executor.clickElement(emailEstimateButton);
+        return new EmailEstimatePage(driver);
+    }
+
+    public String getEstimatedCost() {
+        return estimateCost.getText();
+    }
+}
+
